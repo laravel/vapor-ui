60abf2168691e5c3b2144953fd18a5790d06834c
Nuno Maduro <enunomaduro@gmail.com>
2020-08-31 15:43:27 +0200

    wip

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,21 +10,25 @@
         <!-- Title -->
         <title>Vapor Ui - {{ config('vapor-ui.project') }} - {{ config('vapor-ui.environment') }}</title>
 
-        <!-- Style sheets-->
+        <!-- Style sheets -->
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
         <link href="{{ asset(mix('app.css', 'vendor/vapor-ui')) }}" rel="stylesheet" type="text/css" />
     </head>
     <body>
+        <!-- Vue App-->
         <div id="vapor-ui" class="font-sans antialiased h-screen flex overflow-hidden bg-gray-100" v-cloak>
             <div class="flex flex-shrink-0">
                 <div class="flex flex-col w-64">
                     <div class="flex flex-col h-0 flex-1">
-                        <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-                            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg" alt="Workflow" />
-                        </div>
-                        <div class="flex-1 flex flex-col overflow-y-auto bg-gray-800">
 
-                            <!-- Project / Environment  Information -->
+                        <!-- Logo -->
+                        <div class="flex items-center h-16 px-4 bg-gray-900 text-xl text-white font-medium">
+                            <svg viewBox="0 0 40 40" class="h-12 w-12" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4.345 9h10.55L9.618 20 4.345 9zm21.099 0h10.55l-5.276 11-5.274-11z" fill="#E9F9FD" fill-opacity=".1"/><path fill-rule="evenodd" clip-rule="evenodd" d="M9.62 20h10.549l-5.275 11L9.62 20z" fill="#25C4F2" fill-opacity=".22"/><path fill-rule="evenodd" clip-rule="evenodd" d="M20.169 20h10.55l-5.275 11-5.275-11z" fill="#25C4F2" fill-opacity=".2"/><path fill-rule="evenodd" clip-rule="evenodd" d="M20.169 20H9.619l5.275-11 5.275 11z" fill="#25C4F2" fill-opacity=".4"/><path fill-rule="evenodd" clip-rule="evenodd" d="M30.718 20h-10.55l5.276-11 5.274 11z" fill="#25C4F2" fill-opacity=".4"/><path fill-rule="evenodd" clip-rule="evenodd" d="M25.444 31h-10.55l5.275-11 5.275 11z" fill="#25C4F2" fill-opacity=".5"/><path fill-rule="evenodd" clip-rule="evenodd" d="M3.494 8.467A1 1 0 0 1 4.34 8h10.55a1 1 0 0 1 .902.568l4.373 9.12 4.373-9.12A1 1 0 0 1 25.44 8h10.55a1 1 0 0 1 .902 1.432L26.345 31.424a1.001 1.001 0 0 1-.905.576H14.89a1 1 0 0 1-.902-.568l-10.55-22a1 1 0 0 1 .056-.965zm21.95 2.846L29.13 19h-7.372l3.686-7.687zM5.934 10l3.686 7.687L13.306 10H5.933zm8.96 1.313L18.58 19h-7.372l3.686-7.687zM27.032 10l3.686 7.687L34.405 10h-7.373zm-1.588 18.687L21.758 21h7.372l-3.686 7.687zM23.855 30l-3.686-7.687L16.483 30h7.372zm-8.96-1.313L11.207 21h7.372l-3.686 7.687z" fill="#25C4F2"/></svg>
+                            <div class="ml-2" style="padding-top: 2px;">Laravel Vapor</div>
+                        </div>
+
+                        <div class="flex-1 flex flex-col overflow-y-auto bg-gray-800">
+                            <!-- Project Name -->
                             <div class="px-4 pt-4 space-y-6 sm:flex-row sm:space-y-0 sm:space-x-8 xl:flex-col xl:space-x-0 xl:space-y-6">
                                 <div class="flex items-center space-x-2">
                                     <icon-cloud size="6" class="text-gray-300"></icon-cloud>
@@ -32,6 +36,7 @@
                                 </div>
                             </div>
 
+                            <!-- Project Environment -->
                             <div class="px-4 pt-4 space-y-6 sm:flex-row sm:space-y-0 sm:space-x-8 xl:flex-col xl:space-x-0 xl:space-y-6">
                                 <div class="flex items-center space-x-2">
                                     <icon-flag size="6" class="text-gray-300"></icon-flag>
@@ -92,7 +97,8 @@
                                     Failed
                                 </router-link>
                             </nav>
-                            <!-- Debug tabs -->
+
+                            <!-- Debug tabs / To delete -->
                             <h3 class="px-3 mt-8 text-xs leading-4 font-semibold text-gray-500 uppercase tracking-wider">
                                 Debug
                             </h3>
