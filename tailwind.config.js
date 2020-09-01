bb338d920f1bf4af218bc6ca64d6f5bd8037095a
Nuno Maduro <enunomaduro@gmail.com>
2020-09-01 15:37:00 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: ['./resources/views/*.blade.php'],
+    purge: {
+        content: [
+            './resources/views/**/*.blade.php',
+            './resources/js/**/*.vue',
+            './resources/js/**/*.js'
+        ],
+
+        options: {
+            whitelist: [
+                'text-blue-900',
+                'text-blue-800',
+                'bg-blue-100',
+                'text-yellow-900',
+                'text-yellow-800',
+                'bg-yellow-100',
+                'text-red-900',
+                'text-red-800',
+                'bg-red-100',
+                'text-gray-900',
+                'text-gray800',
+                'bg-gray-100'
+            ],
+        }
+    },
     theme: {
         extend: {
             fontFamily: {
