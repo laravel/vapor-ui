3ff7e4087674225cb0edb5d10ee3e901da72e12b
Nuno Maduro <nunomaduro@users.noreply.github.com>
2020-09-01 10:03:22 +0000

    Apply fixes from StyleCI

--- resources/js/components/SearchDetails.vue
+++ resources/js/components/SearchDetails.vue
@@ -53,9 +53,7 @@
 
             <div v-if="ready && entry" class="max-w-4xl mx-auto">
                 <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                    <div
-                        class="px-4 py-5 border-b border-gray-200 sm:px-6"
-                    >
+                    <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
                         <h3 class="text-lg leading-6 font-medium text-gray-900">
                             {{ $route.meta.title }}
                         </h3>
