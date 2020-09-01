d71c7be67841f54082718d8e20fce9411bdbdb80
Nuno Maduro <nunomaduro@users.noreply.github.com>
2020-09-01 09:35:31 +0000

    Apply fixes from StyleCI

--- resources/js/screens/jobs/show.vue
+++ resources/js/screens/jobs/show.vue
@@ -45,7 +45,10 @@
                 </dd>
             </div>
 
-            <div v-if="entry.content.uuid" class="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
+            <div
+                v-if="entry.content.uuid"
+                class="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5"
+            >
                 <dt class="text-sm leading-5 font-medium text-gray-500">UUID</dt>
                 <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                     {{ entry.content.uuid }}
