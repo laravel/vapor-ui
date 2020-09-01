88c12e09a370b60f4445a7d863547f0e4bde2977
Nuno Maduro <enunomaduro@gmail.com>
2020-09-01 11:38:19 +0200

    wip

--- public/app.js
+++ public/app.js
@@ -3277,6 +3277,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -29328,7 +29331,7 @@ var render = function() {
             _vm._v(" "),
             _c(
               "span",
-              { staticClass: "inline-flex rounded-md shadow-sm" },
+              { staticClass: "inline-flex rounded-md shadow-sm ml-2" },
               [
                 _c(
                   "async-button",
--- resources/js/screens/jobs/show.vue
+++ resources/js/screens/jobs/show.vue
@@ -12,7 +12,7 @@
                     Retry
                 </async-button>
             </span>
-            <span class="inline-flex rounded-md shadow-sm">
+            <span class="inline-flex rounded-md shadow-sm ml-2">
                 <async-button
                     type="button"
                     class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-700 transition ease-in-out duration-150"
