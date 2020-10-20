a50e6273f660d9afe54e522087da0d576a3337e5
Nuno Maduro <enunomaduro@gmail.com>
2020-10-20 18:41:23 +0200

    Updates ignored messages

--- src/Repositories/LogsRepository.php
+++ src/Repositories/LogsRepository.php
@@ -23,11 +23,16 @@ class LogsRepository
      * @var array
      */
     protected $ignore = [
+        'Creating storage directory',
         'START RequestId',
         'REPORT RequestId',
         'END RequestId',
         'Executing warming requests',
-        'Loaded Composer autoload filePreparing to add secrets to runtimePreparing to boot FPMEnsuring ready to start FPMStarting FPM Process',
+        'Loaded Composer autoload file',
+        'Preparing to add secrets to runtime',
+        'Preparing to boot FPM',
+        'Ensuring ready to start FPM',
+        'Starting FPM Process',
         'NOTICE: fpm is running,',
         'NOTICE: ready to handle connections',
         'Caching Laravel configuration',
