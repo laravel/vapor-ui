d87d134a62425639d38297fa50d811528c5c8e49
Nuno Maduro <enunomaduro@gmail.com>
2021-05-31 12:03:00 +0100

    Infers queues from "vapor.yml" file

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -4,28 +4,10 @@ namespace Laravel\VaporUi\Http\Controllers;
 
 use Illuminate\View\View;
 use Laravel\VaporUi\Repositories\QueuesRepository;
+use Laravel\VaporUi\Support\Cloud;
 
 class HomeController
 {
-    /**
-     * Holds an instance of the queue repository.
-     *
-     * @var QueuesRepository
-     */
-    protected $queues;
-
-    /**
-     * Creates a new instance of the job controller.
-     *
-     * @param QueuesRepository $queues
-     *
-     * @return void
-     */
-    public function __construct(QueuesRepository $queues)
-    {
-        $this->queues = $queues;
-    }
-
     /**
      * Returns the Vapor UI home page.
      *
@@ -34,7 +16,7 @@ class HomeController
     public function __invoke()
     {
         return view('vapor-ui::layout', [
-            'queues' => $this->queues->all(),
+            'queues' => Cloud::queues(),
         ]);
     }
 }
